58927027e96a5da9cec08f2eb1a22aa18834cd28
Mickael Daniel <daniel.mickael@gmail.com>
2014-02-01 21:47:38 +0100

    vagrant box & barebone dashboard

---
+++ jenkins/install_jenkins.sh
@@ -0,0 +1,8 @@
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install java-1.6.0-openjdk jenkins -y
+service jenkins start
+chkconfig jenkins on
+
+# For sitespeed.id: phantomjs glibc GConf2 chromedriver chrome firefox (ie &
+# iedriver if windows)
