64adede55984c3590d2fff554fdb0512a0518c7f
Mickael Daniel <daniel.mickael@gmail.com>
2014-02-05 12:54:18 +0100

    Script metrics format to statsd

--- cli/templates/repo/run.sh
+++ cli/templates/repo/run.sh
@@ -10,46 +10,29 @@ WORKSPACE=${WORKSPACE:-$(pwd)}
 GRAPHITE_PREFIX=${GRAPHITE_PREFIX:-'harsummary'}
 # GRAPHITE_SERVER=${GRAPHITE_SERVER:-'dc1-se-prod-admin-02.prod.dc1.kelkoo.net'}
 GRAPHITE_SERVER=${GRAPHITE_SERVER:-'192.168.33.33'}
-GRAPHITE_PORT=${GRAPHITE_PORT:-'2003'}
+GRAPHITE_PORT=${GRAPHITE_PORT:-'8125'}
 
 export DISPLAY=:99.0
 # TODO: Handle Xvfb deamon ?
-sh $DIR/sitespeed.io/bin/sitespeed.io -f $FILE_URLS -a iphone -k true -c firefox
+sh $DIR/sitespeed.io/bin/sitespeed.io -f $FILE_URLS -a iphone -c firefox
 
 # Hopefully enough to get latest created sitespeed-result dir
 LAST=$(ls sitespeed-result/urls.txt/ | tail -n 1)
 HAR_DIR=sitespeed-result/urls.txt/$LAST/data/har
 RESULT_FILE=sitespeed-result/urls.txt/$LAST/data/result.xml
-HARS=$(ls $HAR_DIR)
-
-for file in $HARS; do
-  cp $HAR_DIR/$file /tmp/$file.json
-  url=$(node -pe "require('/tmp/$file.json').log.pages[0].id");
-  domain=$(node -pe "require('url').parse('$url').hostname");
-  protocol=$(node -pe "require('url').parse('$url').protocol");
-  filename=$(echo $file | sed 's/\.har$//')
-  pathname=$(node -pe "require('url').parse('$url').pathname");
-
-  if [[ $pathname == "/" ]]; then
-    pathname="slash"
-  fi
-
-  # From HAR
-  # python $DIR/harstatsgraphite.py -l $domain -p $GRAPHITE_PREFIX.har.$filename.$pathname /tmp/$file.json > /tmp/harstats.txt
-  # cat /tmp/harstats.txt | nc $GRAPHITE_SERVER $GRAPHITE_PORT
-
-  # From browsertime timings:
-
-  # From sitespeed.io XML scores:
-done
 
 # Generate metrics
-npm install
-node $DIR/sitespeed-result-graphite.js $RESULT_FILE $GRAPHITE_PREFIX > /tmp/metrics.txt
+#npm install
+node $DIR/sitespeed-result-graphite.js $RESULT_FILE $GRAPHITE_PREFIX $GRAPHITE_SERVER > /tmp/metrics.txt
 
+# Split every 10 lines, to send smaller packets
+rm -rf /tmp/metrics
+mkdir -p /tmp/metrics
+split -l 10 /tmp/metrics.txt /tmp/metrics/
+FILES=$(ls /tmp/metrics)
 
-while read line
-do
-  echo $line
-  echo $line | nc $GRAPHITE_SERVER $GRAPHITE_PORT
-done < /tmp/metrics.txt
+for file in $FILES; do
+  echo "Sending /tmp/metrics/$file";
+  cat /tmp/metrics/$file
+  cat /tmp/metrics/$file | nc -w 1 -u $GRAPHITE_SERVER $GRAPHITE_PORT
+done
--- cli/templates/repo/sitespeed-result-graphite.js
+++ cli/templates/repo/sitespeed-result-graphite.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 var Url = require('url');
 var exists = fs.existsSync || path.existsSync;
+
 var xml2json = require('xml2js');
 
 var file = process.argv.slice(2)[0];
-var prefix = process.argv.slice(2)[1];
+var prefix = process.argv.slice(2)[1] || 'sitespeed';
 
 if (!exists(file)) {
   console.error('Unable to read file:', file);
@@ -15,6 +16,10 @@ if (!exists(file)) {
 var metricPattern = '%prefix.timings.%browser.%protocol.%path.%metric.%stat';
 var scorePattern = '%prefix.scores.%protocol.%path.%rule';
 var now = Math.floor(Date.now() / 1000);
+
+// Format: graphite or statsd
+var format = 'statsd';
+// var format = 'graphite';
 
 var xml = fs.readFileSync(file, 'utf8');
 xml2json.parseString(xml, function(err, result) {
@@ -39,7 +44,11 @@ function run(data) {
 function getTimingMetrics(metrics) {
   var data = [];
 
+  if (!metrics) return data;
+
   metrics = metrics[0].timingSession;
+
+  if (!metrics) return data;
 
   metrics.forEach(function(m) {
     var stats = m.statistics[0].statistic;
@@ -55,7 +64,7 @@ function getTimingMetrics(metrics) {
     })[0].value[0];
 
     var protocol = Url.parse(url).protocol;
-    var href = Url.parse(url).href.replace(/^https?:\/\//, '').replace(/^\//, '');
+    var href = Url.parse(url).href.replace(/^https?:\/\//, '').replace(/\/$/, '');
 
     var pathname = href.replace(/[\.\/\?\=\+]/g, '-').replace(/(&amp;)/g, '-');
     protocol = protocol.replace(/:/g, '');
@@ -74,7 +83,7 @@ function getTimingMetrics(metrics) {
           .replace('%stat', k);
 
         var value = Math.floor(stat[k]);
-        return [metricPath, value, now].join(' ');
+	return format === 'graphite' ? [metricPath, value, now].join(' ') : metricPath + ':' + value + '|g';
       });
 
       data = data.concat(keys);
@@ -110,131 +119,8 @@ function getScoreMetrics(results) {
         .replace('%path', filename)
         .replace('%rule', g);
 
-      return [metricPath, score, now].join(' ');
-    });
-
-    data = data.concat(metrics);
-  });
-
-  return data;
-}
-
-[kookel@dc1-ci-corp-jenkins-01 poc_perf_r8-monitoring]$ cat sitespeed-result-graphite.js | xclip -i
--bash: xclip: command not found
-[kookel@dc1-ci-corp-jenkins-01 poc_perf_r8-monitoring]$ cat sitespeed-result-graphite.js
-var fs = require('fs');
-var Url = require('url');
-var exists = fs.existsSync || path.existsSync;
-var xml2json = require('xml2js');
-
-var file = process.argv.slice(2)[0];
-var prefix = process.argv.slice(2)[1];
-
-if (!exists(file)) {
-  console.error('Unable to read file:', file);
-  process.exit(1);
-  return;
-}
-
-var metricPattern = '%prefix.timings.%browser.%protocol.%path.%metric.%stat';
-var scorePattern = '%prefix.scores.%protocol.%path.%rule';
-var now = Math.floor(Date.now() / 1000);
-
-var xml = fs.readFileSync(file, 'utf8');
-xml2json.parseString(xml, function(err, result) {
-  if (err) throw err;
-  run(result.document);
-});
-
-function run(data) {
-        // console.log(data);
-	var metrics = data.metrics;
-	var results = data.results;
-
-	var timingMetrics = getTimingMetrics(metrics);
-	var scoreMetrics = getScoreMetrics(results);
-
-	console.log(timingMetrics.join('\n'));
-	console.log(scoreMetrics.join('\n'));
-}
-
-
-/** Function helpers **/
-function getTimingMetrics(metrics) {
-  var data = [];
-
-  metrics = metrics[0].timingSession;
-
-  metrics.forEach(function(m) {
-    var stats = m.statistics[0].statistic;
-
-    var pageData = m.pageData[0].entry;
-
-    var browser = pageData.filter(function(entry) {
-      return entry.key[0] === 'browserName';
-    })[0].value[0];
-
-    var url = pageData.filter(function(entry) {
-      return entry.key[0] === 'url';
-    })[0].value[0];
-
-    var protocol = Url.parse(url).protocol;
-    var href = Url.parse(url).href.replace(/^https?:\/\//, '').replace(/^\//, '');
-
-    var pathname = href.replace(/[\.\/\?\=\+]/g, '-').replace(/(&amp;)/g, '-');
-    protocol = protocol.replace(/:/g, '');
-
-    stats.forEach(function(stat) {
-      var name = stat.name;
-      var keys = Object.keys(stat).filter(function(stat) {
-        return stat !== 'name';
-      }).map(function(k) {
-        var metricPath = metricPattern
-          .replace('%prefix', prefix)
-          .replace('%browser', browser)
-          .replace('%protocol', protocol)
-          .replace('%path', pathname)
-          .replace('%metric', name)
-          .replace('%stat', k);
-
-        var value = Math.floor(stat[k]);
-        return [metricPath, value, now].join(' ');
-      });
-
-      data = data.concat(keys);
-    });
-  });
-
-  return data;
-}
-
-function getScoreMetrics(results) {
-  var data = [];
-
-
-  // If only one page analyzed, results is an Object instead of array
-  results = Array.isArray(results) ? results : [results];
-
-  results.forEach(function(result) {
-    var filename = result['$'].filename;
-    var overall = result.o[0];
-
-    var protocol = Url.parse(result.curl[0]).protocol;
-    protocol = protocol.replace(/:/g, '');
-
-    var grades = result.g[0];
-
-    var metrics = Object.keys(grades).map(function(g) {
-      var d = grades[g][0];
-      var score = d.score[0];
-
-      var metricPath = scorePattern
-        .replace('%prefix', prefix)
-        .replace('%protocol', protocol)
-        .replace('%path', filename)
-        .replace('%rule', g);
-
-      return [metricPath, score, now].join(' ');
+      // node_test.some_service.task.time:500|ms
+      return format === 'graphite' ? [metricPath, score, now].join(' ') : metricPath + ':' + score + '|g';
     });
 
     data = data.concat(metrics);
