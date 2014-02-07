e436a5a21be261102a9b8212f95e2a856a64d303
Mickael Daniel <daniel.mickael@gmail.com>
2014-02-07 18:11:17 +0100

    Change metric path pattern

--- cli/templates/repo/sitespeed-result-graphite.js
+++ cli/templates/repo/sitespeed-result-graphite.js
@@ -13,8 +13,12 @@ if (!exists(file)) {
   return;
 }
 
-var metricPattern = '%prefix.timings.%browser.%protocol.%path.%metric.%stat';
-var scorePattern = '%prefix.scores.%protocol.%path.%rule';
+// var metricPattern = '%prefix.timings.%browser.%protocol.%path.%metric.%stat';
+// var scorePattern = '%prefix.scores.%protocol.%path.%rule';
+
+var scorePattern = '%prefix.%path.scores.%rule';
+var metricPattern = '%prefix.%path.timings.%browser.%metric.%stat';
+
 var now = Math.floor(Date.now() / 1000);
 
 // Format: graphite or statsd
@@ -63,27 +67,28 @@ function getTimingMetrics(metrics) {
       return entry.key[0] === 'url';
     })[0].value[0];
 
-    var protocol = Url.parse(url).protocol;
-    var href = Url.parse(url).href.replace(/^https?:\/\//, '').replace(/\/$/, '');
-
-    var pathname = href.replace(/[\.\/\?\=\+]/g, '-').replace(/(&amp;)/g, '-');
-    protocol = protocol.replace(/:/g, '');
+    var href = Url.parse(url).href;
+    href = href
+      .replace(/\/$/, '')
+      .replace(/^https?:\/\//, '')
+      .replace(/[\.\/\?\=\+]/g, '-')
+      .replace(/(&amp;)/g, '-');
 
     stats.forEach(function(stat) {
       var name = stat.name;
+
       var keys = Object.keys(stat).filter(function(stat) {
         return stat !== 'name';
       }).map(function(k) {
         var metricPath = metricPattern
           .replace('%prefix', prefix)
           .replace('%browser', browser)
-          .replace('%protocol', protocol)
-          .replace('%path', pathname)
+          .replace('%path', href)
           .replace('%metric', name)
           .replace('%stat', k);
 
         var value = Math.floor(stat[k]);
-	return format === 'graphite' ? [metricPath, value, now].join(' ') : metricPath + ':' + value + '|g';
+        return format === 'graphite' ? [metricPath, value, now].join(' ') : metricPath + ':' + value + '|g';
       });
 
       data = data.concat(keys);
@@ -96,15 +101,15 @@ function getTimingMetrics(metrics) {
 function getScoreMetrics(results) {
   var data = [];
 
-
   // If only one page analyzed, results is an Object instead of array
   results = Array.isArray(results) ? results : [results];
 
   results.forEach(function(result) {
     var filename = result['$'].filename;
     var overall = result.o[0];
+    var url = result.curl[0];
 
-    var protocol = Url.parse(result.curl[0]).protocol;
+    var protocol = Url.parse(url).protocol;
     protocol = protocol.replace(/:/g, '');
 
     var grades = result.g[0];
@@ -115,7 +120,6 @@ function getScoreMetrics(results) {
 
       var metricPath = scorePattern
         .replace('%prefix', prefix)
-        .replace('%protocol', protocol)
         .replace('%path', filename)
         .replace('%rule', g);
 
