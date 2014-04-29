b883409f20f461b3dac3f84965e5b67dfd70c5a5
Mickael Daniel <daniel.mickael@gmail.com>
2014-04-29 18:02:50 +0200

    Desired device args options

--- server/scripts/saucelabs-browsertime/index.js
+++ server/scripts/saucelabs-browsertime/index.js
@@ -5,29 +5,8 @@ var path   = require('path');
 process.env.DEBUG = process.env.DEBUG || 'sauce-browsertime';
 var debug  = require('debug')('sauce-browsertime');
 
-// Config
-
-var nopt = require('nopt')({
-  hostname: String,
-  port: Number,
-  browser: String,
-
-});
-
-var username = process.env.SAUCE_USERNAME;
-var accesskey = process.env.SAUCE_ACCESS_KEY;
-
-var webdriverHost = nopt.hostname || 'ondemand.saucelabs.com';
-var webdriverPort = nopt.port || 80;
-
-var url = nopt.argv.remain[0];
-var snippet = "window.performance ? (window.performance.toJSON ? JSON.stringify(window.performance.toJSON()) : window.performance) : '{}'";
-
-
-
-// Based on https://github.com/saucelabs/grunt-init-sauce/blob/master/root/test/sauce/name-specs.js
-
 // checking sauce credential
+
 if (!username || !accesskey) {
   console.warn(
     '\nPlease configure your sauce credential:\n\n' +
@@ -38,7 +17,29 @@ if (!username || !accesskey) {
   throw new Error('Missing sauce credentials');
 }
 
+// Config
+
+var nopt = require('nopt')({
+  hostname: String,
+  port: Number,
+  browser: String,
+  platform: String,
+  'device-type': String,
+  'device-orientation': String,
+  'version': String
+});
+
+var username = process.env.SAUCE_USERNAME;
+var accesskey = process.env.SAUCE_ACCESS_KEY;
+
+var webdriverHost = nopt.hostname || 'ondemand.saucelabs.com';
+var webdriverPort = nopt.port || 80;
+
+var url = nopt.argv.remain[0];
 if (!url) throw new Error('Missing URL(s)');
+
+// Injected script
+var snippet = "window.performance ? (window.performance.toJSON ? JSON.stringify(window.performance.toJSON()) : window.performance) : '{}'";
 
 // http configuration, not needed for simple runs
 wd.configureHttp( {
@@ -47,21 +48,19 @@ wd.configureHttp( {
   retries: 5
 });
 
-// Hmm: http://saucelabs.com/rest/v1/info/browsers/webdriver
 var desired = { browserName: nopt.browser || 'chrome' };
 desired.name = 'Collecting Navigation Timings with ' + desired.browserName;
 desired.tags = ['navtiming'];
+if (nopt.platform) desired.platform = nopt.platform;
+if (nopt.version) desired.version = nopt.version;
+if (nopt['device-type']) desired['device-type'] = nopt['device-type'];
+if (nopt['device-orientation']) desired['device-orientation'] = nopt['device-orientation'];
 
 // Run
 
 var browser = wd.remote(webdriverHost, webdriverPort, username, accesskey);
 
-function done(err) {
-  if (err) throw err;
-}
-
 var urls = nopt.argv.remain.concat();
-
 collect(urls, desired, function(err, data) {
   if (err) return done(err);
   console.log(data);
@@ -71,8 +70,8 @@ collect(urls, desired, function(err, data) {
 function collect(urls, desired, done) {
   var data = [];
   var arr = urls.concat();
-  debug('Init browser', desired);
 
+  debug('Init browser', desired);
   browser.init(desired, function(err, id, caps) {
     if (err) return done(err);
 
@@ -91,7 +90,6 @@ function collect(urls, desired, done) {
       if (!url) return end();
       collectURL(url, function(err, res) {
         if (err) return done(err);
-
         data.push({
           url: url,
           platform: desired,
