97c9458b11d83524c1f5fd5661b6aacb2f036ed6
Mickael Daniel <daniel.mickael@gmail.com>
2014-04-29 18:48:56 +0200

    Using mocha reporters

--- server/scripts/saucelabs-browsertime/index.js
+++ server/scripts/saucelabs-browsertime/index.js
@@ -4,8 +4,12 @@ var wd     = require('wd');
 var path   = require('path');
 process.env.DEBUG = process.env.DEBUG || 'sauce-browsertime';
 var debug  = require('debug')('sauce-browsertime');
+var EventEmitter = require('events').EventEmitter;
 
 // checking sauce credential
+
+var username = process.env.SAUCE_USERNAME;
+var accesskey = process.env.SAUCE_ACCESS_KEY;
 
 if (!username || !accesskey) {
   console.warn(
@@ -26,11 +30,9 @@ var nopt = require('nopt')({
   platform: String,
   'device-type': String,
   'device-orientation': String,
-  'version': String
+  'version': String,
+  reporter: String
 });
-
-var username = process.env.SAUCE_USERNAME;
-var accesskey = process.env.SAUCE_ACCESS_KEY;
 
 var webdriverHost = nopt.hostname || 'ondemand.saucelabs.com';
 var webdriverPort = nopt.port || 80;
@@ -58,24 +60,37 @@ if (nopt['device-orientation']) desired['device-orientation'] = nopt['device-ori
 
 // Run
 
+var runner = new EventEmitter();
+
+var mocha = require('mocha');
+var Test = mocha.Test;
+var Reporter = loadReporter(nopt.reporter);
+var reporter = new Reporter(runner);
+
 var browser = wd.remote(webdriverHost, webdriverPort, username, accesskey);
 
 var urls = nopt.argv.remain.concat();
 collect(urls, desired, function(err, data) {
   if (err) return done(err);
-  console.log(data);
-  console.log('end');
 });
+
 
 function collect(urls, desired, done) {
   var data = [];
   var arr = urls.concat();
+
+
+  runner.emit('start');
+  runner.emit('suite', { title: desired.name });
 
   debug('Init browser', desired);
   browser.init(desired, function(err, id, caps) {
     if (err) return done(err);
 
     function end() {
+      runner.emit('suite end', { title: desired.name });
+      runner.emit('end', { title: desired.name });
+
       debug('Ending session %s', id);
       browser.quit(function(err) {
         if (err) return done(err);
@@ -104,6 +119,11 @@ function collect(urls, desired, done) {
 
 function collectURL(url, done) {
   debug('Getting %s url', url);
+
+  var test = new Test(desired.name + ' - ' + url);
+  test.parent = { fullTitle: function() { return url; } };
+
+  runner.emit('test', test);
   browser.get(url, function(err) {
     if (err) return done(err);
 
@@ -114,7 +134,25 @@ function collectURL(url, done) {
       var data = typeof res === 'string' ? res :
         JSON.stringify(res.timing, null, 2);
 
+      runner.emit('pass', test);
+      runner.emit('test end', test);
       done(null, data);
     });
   });
+}
+
+// Loading helpers for Mocha reporters
+function loadReporter(reporter) {
+  if (typeof reporter === 'function') return reporter;
+
+  reporter = reporter || 'json';
+  var _reporter;
+  try { _reporter = require('mocha/lib/reporters/' + reporter); } catch (err) {}
+  if (!_reporter) try { _reporter = require(reporter); } catch (err) {}
+  if (!_reporter && reporter === 'teamcity')
+    console.warn('The Teamcity reporter was moved to a package named ' +
+      'mocha-teamcity-reporter ' +
+      '(https://npmjs.org/package/mocha-teamcity-reporter).');
+  if (!_reporter) throw new Error('invalid reporter "' + reporter + '"');
+  return _reporter;
 }
