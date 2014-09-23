b8d7a9c0c330a1e84dd166e57659a805f1b21596
Mickael Daniel <daniel.mickael@gmail.com>
2014-09-23 15:52:55 +0200

    phantomas - mailer daily / weekly

--- s4/lib/models/jobs/mailer-daily.js
+++ s4/lib/models/jobs/mailer-daily.js
@@ -3,13 +3,13 @@ var path = require('path');
 var util = require('util');
 var Base  = require('./base');
 
-var xml = fs.readFileSync(path.join(__dirname, 'mailer.xml'), 'utf8');
+var xml = fs.readFileSync(path.join(__dirname, 'mailer-daily.xml'), 'utf8');
 
 module.exports = Mailer;
 
 function Mailer() {
   Base.apply(this, arguments);
-  this.set('name', 'mailer');
+  this.set('name', 'mailer-daily');
   this.set('xml', xml);
 }
 
