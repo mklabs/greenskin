97a3768ff2e40e6390a01fb6b5a014c61b0eb3df
Mickael Daniel <daniel.mickael@gmail.com>
2016-04-18 02:52:10 +0200

    List jobs from agenda api, implement helpers, partials

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 let app = require('./lib/app');
 let greenskin = module.exports = app.greenskin;
 
-greenskin.app      = app;
-greenskin.agenda   = require('./lib/agenda');
-greenskin.agendash = require('./lib/agendash');
-
 greenskin.listen = (opts) => {
   return new Promise((r, errback) => {
+    greenskin.options = opts || {};
+    greenskin.collection = opts.collection || 'jobs';
+
     return greenskin.connect(opts.db)
       .catch(errback)
       .then(function() {
