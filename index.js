417c16524c461640ee673f22149cc80e09691062
Mickael Daniel <daniel.mickael@gmail.com>
2016-04-20 00:47:54 +0200

    Implement recovery of past job on startup

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 let app = require('./lib/app');
 let moment = require('moment');
+let tz = require('moment-timezone');
+let humanInterval = require('human-interval');
+
+let debug = require('debug')('greenskin');
+
 let greenskin = module.exports = app.greenskin;
 
 greenskin.listen = (opts) => {
@@ -19,17 +24,44 @@ greenskin.listen = (opts) => {
 };
 
 greenskin.start = (opts) => {
-  return greenskin.listen(opts)
-    .then(() => {
-      return greenskin.query()
-        .then((jobs) => {
-          jobs = jobs.filter((job) => {
-            var now = moment();
-            var nextRunAt = moment(Date.parse(job.nextRunAt));
-            if (nextRunAt.isBefore(now)) {
-              greenskin.agenda.every(job.repeatInterval, job.name);
-            }
-          });
+  return greenskin.listen(opts).then(() => {
+    return new Promise((r, errback) => {
+      greenskin.agenda.jobs({}, (err, jobs) => {
+        if (err) return errback(err);
+
+        jobs = jobs.filter((job) => {
+          var now = moment();
+          var nextRunAt = moment(Date.parse(job.attrs.nextRunAt));
+          return nextRunAt.isBefore(now) && job.attrs.type === 'single';
         });
+
+        if (!jobs.length) return r();
+
+        return greenskin.recover(jobs)
+          .catch(errback)
+          .then(r);
+      })
     });
+  });
+};
+
+greenskin.recover = (jobs) => {
+  var promises = jobs.map(greenskin.recoverJob);
+  debug('Recover %d jobs', jobs.length);
+  return Promise.all(promises);
+};
+
+greenskin.recoverJob = (job) => {
+  return new Promise((r, errback) => {
+    var nextRunAt = new Date(job.attrs.nextRunAt).getTime();
+    var lastRun = tz();
+    if (job.attrs.repeatTimezone) lastRun = d.tz(job.attrs.repeatTimezone);
+
+    job.attrs.nextRunAt = lastRun.valueOf() + humanInterval(job.attrs.repeatInterval);
+    job.save((err) => {
+      if (err) return errback(err);
+      debug('Updated %s job. Next run: %s', job.attrs.name, moment(job.attrs.nextRunAt).format('LLLL'));
+      r();
+    });
+  });
 };
